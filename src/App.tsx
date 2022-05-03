6bf7a3cbf92e83c8a1763722274d119fd8394dc9
larissa.fiorini <larissa.fiorini@hotmail.com>
2022-05-03 18:35:38 -0300

    Finish day1

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import { useState } from 'react'
+import {Widget} from './components/Widget';
 
-interface ButtonProps{
-  text?:string;
-}
-
-function Button(props: ButtonProps){
-  return <button className='button'> {props.text ?? 'Default' }</button>
-}
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App(){
   return (
-    <div className='flex gap-2'>
-      <Button text="Enviar"/>
-      <Button text="Ok"/>
-    </div>
-    )
+    <Widget />
+  )
 }
-
-export default App
